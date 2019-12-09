5615af569493522eff9625327c12429b03fdcd94
Konrad <konradorlecki6@wp.pl>
2019-12-09 10:51:05 +0100

    home layout, search city

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {HomePageComponent} from '../home-page/home-page.component';
 
 
 const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', component: HomePageComponent},
 ];
 
--- src/home-page/home-page.component.ts
+++ src/home-page/home-page.component.ts
@@ -17,8 +17,13 @@ export class HomePageComponent implements OnInit {
   }
 
   searchCity() {
-    this.http.get(`//api.openweathermap.org/data/2.5/weather?q=poznan&APPID=9255e12d14f3883315c070a9d1899c07`)
-      .subscribe(data => this.cities.push(data));
+    this.http.get(`//api.openweathermap.org/data/2.5/weather?q=${this.cityName}&APPID=9255e12d14f3883315c070a9d1899c07`)
+      .subscribe(data => {
+        // @ts-ignore
+        data.main.temp = Math.round(data.main.temp - 273) + '°C';
+        this.cities.push(data);
+        console.log(data);
+      });
 
   }
 }
